e86ef4f88963f594e84ec7b2f1b18e043f3d8fd1
Thiago2104 <santijipin@gmail.com>
2021-11-09 23:55:37 -0500

    Implements Firebase project

--- src/Components/ShipCreator/ShipFactory.js
+++ src/Components/ShipCreator/ShipFactory.js
@@ -1,40 +1,40 @@
 const ShipFactory = function ()
 {
-    this.createShip= function (tipo, coord, orientation, state){
+    this.createShip= function (tipo, coord, orientation){
         const ship;
         if(tipo==2){
-            ship= new Lancha(coord, orientation, state);
+            ship= new Lancha(coord, orientation);
         }else if(tipo==3){
-            ship= new Submarino(coord, orientation, state);
+            ship= new Submarino(coord, orientation);
         }else if(tipo==4){
-            ship= new Fragata(coord, orientation, state);
+            ship= new Fragata(coord, orientation);
         }else if(tipo==5){
-            ship= new Lancha(coord, orientation, state);
+            ship= new Lancha(coord, orientation);
         }else {
             ship= new NullShip();
         }
         return ship;
     }
 }
-const Lancha= function (coord, orientation, state)
+const Lancha= function (coord, orientation)
 {
     this.coords= coord;
     this.orientation = orientation;
     this.state = [0,0];
 }
-const Fragata = function(coord, orientation, state)
+const Fragata = function(coord, orientation)
 {
     this.coords= coord;
     this.orientation = orientation;
     this.state = [0,0,0,0];
 }
-const Portaaviones = function(coord, orientation, state)
+const Portaaviones = function(coord, orientation)
 {
     this.coords= coord;
     this.orientation = orientation;
     this.state = [0,0,0,0,0];
 }
-const Submarino= function(coord, orientation, state)
+const Submarino= function(coord, orientation)
 {
     this.coords= coord;
     this.orientation = orientation;
