cb7a85c8a7fdf0c5e0f4ddac0b8165b3ce241248
Thiago2104 <santijipin@gmail.com>
2021-11-09 21:44:34 -0500

    Create ShipFactory.js

---
+++ src/Components/ShipCreator/ShipFactory.js
@@ -0,0 +1,48 @@
+const ShipFactory = function ()
+{
+    this.createShip= function (tipo, coord, orientation, state){
+        const ship;
+        if(tipo==2){
+            ship= new Lancha(coord, orientation, state);
+        }else if(tipo==3){
+            ship= new Submarino(coord, orientation, state);
+        }else if(tipo==4){
+            ship= new Fragata(coord, orientation, state);
+        }else if(tipo==5){
+            ship= new Lancha(coord, orientation, state);
+        }else {
+            ship= new NullShip();
+        }
+        return ship;
+    }
+}
+const Lancha= function (coord, orientation, state)
+{
+    this.coords= coord;
+    this.orientation = orientation;
+    this.state = state;
+}
+const Fragata = function(coord, orientation, state)
+{
+    this.coords= coord;
+    this.orientation = orientation;
+    this.state = state;
+}
+const Portaaviones = function(coord, orientation, state)
+{
+    this.coords= coord;
+    this.orientation = orientation;
+    this.state = state;
+}
+const Submarino= function(coord, orientation, state)
+{
+    this.coords= coord;
+    this.orientation = orientation;
+    this.state = state;
+}
+const NullShip = function()
+{
+    this.coords= [[0,0],[0,0]];
+    this.orientation= "vertical";
+    this.state= [];
+}
